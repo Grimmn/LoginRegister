c7521e3d591790dce6052b2b0a3afd93e4573868
Utku Enes Alagöz <utkuenesalagoz@icloud.com>
2022-09-19 13:37:10 +0300

    Initial Commit

---
+++ BeraTasarım3/BeraTasar_m3App.swift
@@ -0,0 +1,17 @@
+//
+//  BeraTasar_m3App.swift
+//  BeraTasarım3
+//
+//  Created by utku enes alagöz on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeraTasar_m3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeraTasarım3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeraTasarım3
+//
+//  Created by utku enes alagöz on 19.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
